bfd37dff0729d36c248d91ad675666276b5ae248
Drewry Pope <drewrypope@gmail.com>
2020-07-02 00:11:23 -0500

    add license notices

--- text2png.js
+++ text2png.js
@@ -1,3 +1,8 @@
+/* Copyright 2020 Drewry Pope
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 var fs = require('fs');
 var text2png = require('text2png');
 fs.writeFileSync('out.png', text2png('Hello! Potato.', {color: 'blue'}));
