46fade9daf409ae20a57d3051bfd3a7ec38ddf35
Drewry Pope <drewrypope@gmail.com>
2020-07-02 02:17:23 -0500

    Add build/deploy scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-FROM node AS base
-ENV NPM_CONFIG_LOGLEVEL info
+#!/bin/bash
 
-FROM base AS build
-COPY . .
-RUN /bin/bash build.sh
-
-FROM build AS deploy
-RUN /bin/bash entrypoint.sh
+. ./text2png.js
 
