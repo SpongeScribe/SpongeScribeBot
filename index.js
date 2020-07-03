753407978e0adb284dbca37cd5ffb89a17bb942d
Drewry Pope <drewrypope@gmail.com>
2020-07-03 05:08:46 -0500

    update package.json script

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-var fs = require('fs');
-var text2png = require('text2png');
-fs.writeFileSync('out.png', text2png(process.env.IMAGE_TEXT == null || process.env.IMAGE_TEXT == '' ? 'Hello! Potato.' : process.env.IMAGE_TEXT, {color: 'blue'}));
+let fs = require('fs');
+let text2png = require('text2png');
+let imageText = process.env.IMAGE_TEXT || 'Hello!\nPotato.'
+fs.writeFileSync('out.png', text2png(imageText, {color: 'blue'}));
