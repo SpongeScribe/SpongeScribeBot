390590718df0e924f39fd51ba3525172c89d06c3
Drewry Pope <drewrypope@gmail.com>
2020-07-03 02:59:56 -0500

    parameterize image

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
 # Copyright 2020 Drewry Pope
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#!/bin/bash
-
 set -ex
 
 rm -f ./version;
 touch version;
+
 printf "node=" >> version;
 node -v >> version;
+
 printf "npm=" >>version;
 npm -v >> version;
+
 printf "app=" >>version;
 cat appversion >> version;
-cat version; 
 
+cat version;
