c3abbf89e13a18078a829f0fec9d681a64add108
Drewry Pope <drewrypope@gmail.com>
2020-07-03 01:09:37 -0500

    license files, improve parameters

--- get-image.sh
+++ get-image.sh
@@ -1,1 +1,8 @@
-docker cp $(docker run -d $(docker build . -q) $1; sleep 1):/usr/local/app/out.png ./out.png
+# Copyright 2020 Drewry Pope
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#!/bin/bash
+
+docker cp $(docker run -d $(docker build . -q) entrypoint.sh $1; sleep 1):/usr/local/app/out.png ./out.png
