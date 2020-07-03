a5f7122feeb7218a647010ac265e905c18795e56
Drewry Pope <drewrypope@gmail.com>
2020-07-03 01:25:23 -0500

    work toward parameterization

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 var fs = require('fs');
-var text2png = require('./node_modules/text2png');
-fs.writeFileSync('out.png', text2png('Hello! Potato.', {color: 'blue'}));
+var text2png = require('text2png');
+fs.writeFileSync('out.png', text2png(process.env.IMAGE_TEXT == null || process.env.IMAGE_TEXT == '' ? 'Hello! Potato.' : process.env.IMAGE_TEXT, {color: 'blue'}));
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
