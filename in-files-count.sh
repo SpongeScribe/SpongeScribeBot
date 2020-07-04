aa5ef7f71483ba142d250a24ad5c11d1d6f017e1
Drewry Pope <drewrypope@gmail.com>
2020-07-04 00:00:51 -0500

    CODE_OF_CONDUCT
    IMPORTANT NOTE:
    Please note that this project is released with a
    Contributor Code of Conduct, located in the root
    directory of this project under the file name:
    
     - `CODE_OF_CONDUCT.md`
    
    By participating in this project you agree to abide by its terms.
    
    Thank you for your participation.
    
    https://github.com/SpongeScribe/SpongeScribeBot/blob/main/CODE_OF_CONDUCT.md

--- in-files-count.sh
+++ in-files-count.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+# Author: Drewry Pope
+# Any copyright is dedicated to the Public Domain.
+# https://creativecommons.org/publicdomain/zero/1.0/
 echo "{ \"count\" : [ { \"json\" : \"$(ls ./appdata/in/*.json 2> /dev/null | wc -l)\" }, { \"png\" : \"$(ls ./appdata/in/*.png 2> /dev/null | wc -l)\" } ] }"
