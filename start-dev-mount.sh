3aff4587b62c9a83c0cd1b57a189fd34ec50d299
Drewry Pope <drewrypope@gmail.com>
2020-07-03 19:35:14 -0500

    setup for mount

---
+++ start-dev-mount.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Any copyright is dedicated to the Public Domain.
+# https://creativecommons.org/publicdomain/zero/1.0/
+
+set -ex
+
+docker run -v $PWD/appdata/in:/usr/local/app/data/in -v $PWD/appdata/out:/usr/local/app/data/out -it $(docker build app -q --target build) $*
