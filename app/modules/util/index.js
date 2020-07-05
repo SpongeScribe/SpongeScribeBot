bc9d150f4750c8e419283341805a1955a3f3fde1
Drewry Pope <drewrypope@gmail.com>
2020-07-05 03:42:25 -0500

    cleanup git repo since everything is on fire
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

--- app/modules/util/index.js
+++
@@ -1,7 +0,0 @@
-// Author: Drewry Pope
-/* Any copyright is dedicated to the Public Domain.
- * https://creativecommons.org/publicdomain/zero/1.0/ */
-
-export async function sleep (ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
