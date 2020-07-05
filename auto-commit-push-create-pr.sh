69d0443f78733254f2183e3e79cf0ce5b82e7397
Drewry Pope <drewrypope@gmail.com>
2020-07-05 10:40:23 -0500

    test almost done

--- auto-commit-push-create-pr.sh
+++ auto-commit-push-create-pr.sh
@@ -3,7 +3,7 @@
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
 if  [ "$1" = "--headless" ]; then
-	HEADLESS=0
+	HEADLESS=1
 	shift
 fi
 git add . 2>/dev/null
@@ -13,7 +13,7 @@ branch_name=${branch_name##refs/heads/}
 if  [ -n "$branch_name" ]; then
 	git push --set-upstream origin $branch_name
 	gh pr create --fill
-	if  [ "$HEADLESS" = 0 ]; then
+	if  [ -z $HEADLESS ]; then
 		gh pr view --web
 	fi
 fi
