bb2c6cfe0ec4f21c35ecfabccc738bf82dc281b6
Drewry Pope <drewrypope@gmail.com>
2020-07-05 10:20:23 -0500

    improvements

--- auto-commit-push-create-pr.sh
+++ auto-commit-push-create-pr.sh
@@ -9,11 +9,12 @@ git add . 2>/dev/null
 git commit -m "$@" 2>/dev/null
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
-
 if  [ -n "$branch_name" ]; then
 	git push --set-upstream origin $branch_name 2>/dev/null
 	gh pr create --fill  2>/dev/null
 	if  [ "$HEADLESS" ]; then
-		gh pr view --web 2>/dev/null # same as link output, don't include in headless scripts, autoselect pr based on your branch
+		gh pr view --web
+	else
+		echo ' all good'
 	fi
 fi
