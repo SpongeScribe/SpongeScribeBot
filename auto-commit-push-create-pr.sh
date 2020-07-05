51f57997ba2ff5f53d43dc6992d732e7a8521859
Drewry Pope <drewrypope@gmail.com>
2020-07-05 10:34:49 -0500

    test almost done

--- auto-commit-push-create-pr.sh
+++ auto-commit-push-create-pr.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 # Author: Drewry Pope
+# Any copyright is dedicated to the Public Domain.
+# https://creativecommons.org/publicdomain/zero/1.0/
 if  [ "$1" = "--headless" ]; then
 	HEADLESS=0
 	shift
 fi
 git add . 2>/dev/null
-git commit -m "$@" 2>/dev/null
+git commit -am "$*"
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
 if  [ -n "$branch_name" ]; then
-	git push --set-upstream origin $branch_name 2>/dev/null
+	git push --set-upstream origin $branch_name &>/dev/null
 	gh pr create --fill  2>/dev/null
 	if  [ "$HEADLESS" = 0 ]; then
-		echo "1" > out # gh pr view --web
-
-	else
-		echo "22" > out # gh pr view --web
+		gh pr view --web 2>/dev/null
 	fi
 fi
