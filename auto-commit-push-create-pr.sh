3b952346c3f903cfa3d5499a77e21da4d385fc61
Drewry Pope <drewrypope@gmail.com>
2020-07-05 10:18:26 -0500

    improvements

--- auto-commit-push-create-pr.sh
+++ auto-commit-push-create-pr.sh
@@ -9,6 +9,7 @@ git add . 2>/dev/null
 git commit -m "$@" 2>/dev/null
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
+
 if  [ -n "$branch_name" ]; then
 	git push --set-upstream origin $branch_name 2>/dev/null
 	gh pr create --fill  2>/dev/null
