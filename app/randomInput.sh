beadf88ab4d07dde2a792caab91c691307b5d736
Drewry Pope <drewrypope@gmail.com>
2020-07-05 17:15:43 -0500

    sometimes runs a picture
    but also is pretty broken,
    mostly working promises/async,
    moved a file with limited errors
    added some todo
    babel and native ecma both 'working'
    whatever these do: --unhandled-rejections=strict --trace-warnings
    work on docker allow
    work on version script
    work on bash scripts
    add randominput into container
    
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

---
+++ app/randomInput.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2020 Drewry Pope
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+set -ex
+if [ -z $IMAGE_TEXT]; then
+	IMAGE_TEXT="Hello!\nPotato."
+fi
+
+FILENAME=./data/in/input.random.`uuidgen`.json
+echo "{
+    \"items\": [
+        { \"username\" : \"TEST\", \"imageText\" : \"$IMAGE_TEXT\" }" > $FILENAME;
+for run in {1..10}; do
+	USERNAME=`uuidgen`
+	for run in {1..100}; do
+		echo "        , { \"username\" : \"$USERNAME\", \"imageText\" : \"$(uuidgen)\" }" >> $FILENAME;
+	done
+done
+echo "    ]" >> $FILENAME
+echo "}" >> $FILENAME
