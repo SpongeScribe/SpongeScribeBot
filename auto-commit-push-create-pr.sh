8a257f6c2ce3493c271c59d5ee86ef7f0e359a03
Drewry Pope <drewrypope@gmail.com>
2020-07-05 10:44:38 -0500

    test333 (#15)
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * improvements
    
    * test almost done
    
    * test almost done
    
    * test almost done
    
    * test almost done
    
    * test almost done
    
    * test almost done

--- auto-commit-push-create-pr.sh
+++ auto-commit-push-create-pr.sh
@@ -2,26 +2,18 @@
 # Author: Drewry Pope
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
-if  [ "$1" == "--headless" ]; then
+if  [ "$1" = "--headless" ]; then
 	HEADLESS=0
 	shift
-else
-	HEADLESS=1
 fi
-git add .
-git commit -m "$@"
+git add . 2>/dev/null
+git commit -am "$*"
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
 if  [ -n "$branch_name" ]; then
 	git push --set-upstream origin $branch_name
-	PR_URL=gh pr create --fill
-
-	if  [ $HEADLESS -eq 0 ]; then
-		gh pr view --web # same as link output, don't include in headless scripts, autoselect pr based on your branch
-    else
-    	echo 'Headless mode. Skipping "gh pr view"'
+	gh pr create --fill
+	if  [ -z $HEADLESS ]; then
+		gh pr view --web
 	fi
-else
-	echo "ERROR: no branch found. Are you detached?"
 fi
-return PR_URL
