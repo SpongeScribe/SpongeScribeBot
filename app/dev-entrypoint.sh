636a74f17790d044384021d5817cec70b1bf007e
Drewry Pope <drewrypope@gmail.com>
2020-07-05 03:35:47 -0500

    the new stuff thats broken too
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

---
+++ app/dev-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright 2020 Drewry Pope
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+set -ex
+IMAGE_TEXT="$*"
+export IMAGE_TEXT=$IMAGE_TEXT
+echo "IMAGE_TEXT=$IMAGE_TEXT" | tee --append .env /etc/environment
+if [ ! -z $IMAGE_TEXT ]; then
+    FILENAME=./data/in/input.manual.`uuidgen`.json
+    echo "{
+        \"items\": [
+            { \"username\" : \"@@MANUAL\", \"imageText\" : \"$IMAGE_TEXT\" }" > $FILENAME;
+    echo "    ]" >> $FILENAME
+    echo "}" >> $FILENAME
+fi
+cat $FILENAME
+
+. ./entrypoint.sh
---
+++ app/twitter.js
@@ -0,0 +1,18 @@
+import twitter from 'twitter-lite';
+const client = new Twitter({
+    subdomain: "api", // "api" is the default (change for other subdomains)
+    version: "1.1", // version "1.1" is the default (change for other subdomains)
+    consumer_key: process.env.TWITTER_API_KEY, // from Twitter.
+    consumer_secret: process.env.TWITTER_API_SECRET_KEY, // from Twitter.
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN, // from your User (oauth_token)
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET // from your User (oauth_token_secret)
+});
+
+client
+    .get("account/verify_credentials")
+    .then(
+        results => {
+            console.log("results", results);
+        }
+    )
+    .catch(console.error);
