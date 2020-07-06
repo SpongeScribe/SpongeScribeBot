7612e037bd27087371a0dc1e52fd1314dff3d4dd
Drewry Pope <drewrypope@gmail.com>
2020-07-05 21:54:41 -0500

    reorg & break things
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

---
+++ app/twitter.autohook.js
@@ -0,0 +1,22 @@
+import pkg from 'twitter-autohook';
+const { Autohook } = pkg;
+
+
+(async start => {
+  try {
+    const webhook = new Autohook();
+    
+    // Removes existing webhooks
+    await webhook.removeWebhooks();
+    
+    // Starts a server and adds a new webhook
+    await webhook.start();
+    
+    // Subscribes to your own user's activity
+    await webhook.subscribe({oauth_token: process.env.TWITTER_ACCESS_TOKEN, oauth_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET});  
+  } catch (e) {
+    // Display the error and quit
+    console.error(e);
+    process.exit(1);
+  }
+})();
