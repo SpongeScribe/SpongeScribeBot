7c35179ec56cefc56bc69927562199c1c45699b9
Drewry Pope <drewrypope@gmail.com>
2020-07-07 19:12:51 -0500

    cleanup script namespace
    Signed-off-by: Drewry Pope <drewrypope@gmail.com>

---
+++ scripts/etc/create-data-dirs-and-symlinks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Drewry Pope
+# Any copyright is dedicated to the Public Domain.
+# https://creativecommons.org/publicdomain/zero/1.0/
+set -x
+`pwd`/scripts/etc/delete-symlinks.sh
+mkdir -p `pwd`/data/in
+mkdir -p `pwd`/data/out
+ln -s `pwd`/app/* `pwd`
+ln -s `pwd`/app/.* `pwd`
+ln -s `pwd`/app/scripts/* `pwd`/scripts
+ln -s `pwd`/data `pwd`/app/data
