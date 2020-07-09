4509636cb3fe3acfcd1ec7a87803373c5494c5df
Drewry Pope <drewrypope@gmail.com>
2020-07-09 01:15:47 -0500

    fights

--- scripts/etc/append-symlinks-to-gitignore.sh
+++ scripts/etc/append-symlinks-to-gitignore.sh
@@ -3,6 +3,6 @@
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
 set -x
-`pwd`/scripts/etc/create-data-dirs-and-symlinks.sh
+./scripts/etc/create-data-dirs-and-symlinks.sh
 printf "\n# symlinks added by \`./scripts/etc/append-symlinks-to-gitignore.sh\`\n" >> .gitignore;
 find . -type l | sed 's|^./||' | xargs -I {} -n1 /bin/echo "/{}" >> .gitignore
--- scripts/etc/create-data-dirs-and-symlinks.sh
+++ scripts/etc/create-data-dirs-and-symlinks.sh
@@ -3,23 +3,21 @@
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
 set -x
-mkdir -p `pwd`/scripts/etc
-touch `pwd`/scripts/etc/delete-symlinks.sh
-`pwd`/scripts/etc/delete-symlinks.sh
-mkdir -p `pwd`/data/in
-mkdir -p `pwd`/data/out
-find -type f -xtype f -not -path "./.git*" -name '*.sh' -exec sh -c 'ln -s ./$(basename {}) `pwd`/$(echo "{}" | rev | cut -c 4- | rev)' \;
-mkdir -p `pwd`/app/scripts
-ln -s ./app/* `pwd`
-ln -s ./app/.* `pwd`
-ln -s ../app/scripts/ `pwd`/scripts/scripts
+mkdir -p scripts/etc
+touch scripts/etc/delete-symlinks.sh
+./scripts/etc/delete-symlinks.sh
+mkdir -p data/in
+mkdir -p data/out
+find -type f -xtype f -not -path "./.git*" -name '*.sh' -exec sh -c 'ln -s $(basename {}) $(echo "{}" | rev | cut -c 4- | rev)' \;
+mkdir -p app/scripts
+ln -s app/* `pwd`
+ln -s app/.* `pwd`
+ln -s ../app/scripts/ scripts/scripts
 cd scripts
-ln -s ./scripts/* `pwd`
+ln -s scripts/* `pwd`
 cd ..
-ln -s ./scripts/* `pwd`
-mkdir -p `pwd`app/data
-ln -s `pwd`/data `pwd`/app/data
-
-find -type l -not -path "./.git*" -name '*.sh' -exec sh -c 'cp -P ./{} `pwd`/$(echo "{}" | rev | cut -c 4- | rev)' \;
-
-ln -s ./scripts/etc/create-data-dirs-and-symlinks.sh init
+ln -s scripts/* `pwd`
+ln -s ../data app/data
+find -type l -not -path "./.git*" -name '*.sh' -exec sh -c 'cp -P ./{} $(echo "{}" | rev | cut -c 4- | rev)' \;
+ln -s package.json package
+ln -s scripts/etc/create-data-dirs-and-symlinks.sh init
