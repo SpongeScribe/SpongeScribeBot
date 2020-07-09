8114b32221818db96e30c59ed4683e5ca3db64db
Drewry Pope <drewrypope@gmail.com>
2020-07-08 23:28:55 -0500

    update gitignore

--- scripts/etc/append-symlinks-to-gitignore.sh
+++ scripts/etc/append-symlinks-to-gitignore.sh
@@ -3,6 +3,6 @@
 # Any copyright is dedicated to the Public Domain.
 # https://creativecommons.org/publicdomain/zero/1.0/
 set -x
-`pwd`/scripts/create-data-dirs-and-symlinks.sh
+`pwd`/scripts/etc/create-data-dirs-and-symlinks.sh
 printf "\n# symlinks added by \`./scripts/init-local-app-symlinks.sh\`\n" >> .gitignore;
 find . -type l | sed 's|^./||' >> .gitignore
