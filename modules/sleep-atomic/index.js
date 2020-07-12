e38dfcaa74bb6ce1a02990a2fc60758d77332c1d
Drewry Pope <drewrypope@gmail.com>
2020-07-12 09:33:40 -0500

    sleep-atomic@0.9.3

--- modules/sleep-atomic/index.js
+++ modules/sleep-atomic/index.js
@@ -4,8 +4,7 @@
 export default async function sleep (ms) {
     return new Promise(resolve => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms));
 }
-export default async function sleepWithLog (ms) {
+export async function sleepWithLog (ms) {
     console.log('{ "sleep" : { "parameters: [ { "ms" : "' + ms + '" } ] } }' );
     return new Promise(resolve => Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms));
 }
-
